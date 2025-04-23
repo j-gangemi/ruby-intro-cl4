a377940e4f863f2023d327728e2d6bfc8b859967
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-04-23 15:55:12 +0000

    codealongs 1 and 2

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,37 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+puts 5
+
+puts 3+1 
+puts 5*2
+
+puts 3.0/5.0
 
 # Non-numbers
+puts "3+5"
 
 # True, False
+puts true
+puts false
 
 # Nothing
 
 # Variables
+x = 3
+y = 5
+puts x
+puts y
+puts x + y
+z = 3 + 4
+puts z
+puts e = "Tacos"
+puts tacos = "Tacos"
 
 # Combine strings and variables
+puts "Tacos on today's menu: #{x}"
+puts "#{e} on today's menu: #{x}"
+puts "#{tacos} on today's menu: #{x}"
 
 # String manipulation
